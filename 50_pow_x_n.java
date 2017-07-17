df561b4fdf08bc71e257f97431b387d27f4f4b30
Fan Yiming <alkalidic@gmail.com>
2017-07-17 14:14:49 +0800

    50

---
+++ 50_pow_x_n.java
@@ -0,0 +1,14 @@
+public class Solution {
+  double myPow(double x, int n) {
+    if(n<0) return 1/x * myPow(1/x, -(n+1));
+    if(n==0) return 1;
+    if(n==2) return x*x;
+    if(n%2==0) return myPow( myPow(x, n/2), 2);
+    else return x*myPow( myPow(x, n/2), 2);
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.myPow(3.0, 2));
+  }
+}
